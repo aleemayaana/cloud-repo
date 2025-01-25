5bd7279fd5236232334428049d6e225ae21bb746
aleemayaana <107088988+aleemayaana@users.noreply.github.com>
2025-01-25 10:11:17 -0500

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,9 @@
+variable "access_key_id" {
+  description = "AWS Access Key ID"
+  type        = string
+}
+
+variable "secret_access_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+}
